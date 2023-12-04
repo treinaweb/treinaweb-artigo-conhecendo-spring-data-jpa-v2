7cb33bbd32abd937215123826f3e7bed1c381dd8
Cleyson <cleysonph@gmail.com>
2023-12-03 21:59:19 -0300

    04:16 - Relacionamento entre as entidades Employee e Position

--- src/main/java/br/com/treinaweb/twprojects/core/models/Employee.java
+++ src/main/java/br/com/treinaweb/twprojects/core/models/Employee.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -55,5 +56,8 @@ public class Employee {
 
     @OneToOne(optional = false, cascade = CascadeType.ALL, orphanRemoval = true)
     private Address address;
+
+    @ManyToOne(optional = false)
+    private Position position;
     
 }
