0b648123fc0504bde169004e3d72ec68b1a16b9d
Cleyson <cleysonph@gmail.com>
2023-12-04 05:46:01 -0300

    07:24 - Entendo o problema N+1 e o resolvendo

--- src/main/java/br/com/treinaweb/twprojects/core/repositories/EmployeeRepository.java
+++ src/main/java/br/com/treinaweb/twprojects/core/repositories/EmployeeRepository.java
@@ -1,9 +1,19 @@
 package br.com.treinaweb.twprojects.core.repositories;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.treinaweb.twprojects.core.models.Employee;
 
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+    @EntityGraph(attributePaths = "position")
+    List<Employee> findAll();
+
+    @EntityGraph(attributePaths = { "position", "address", "projects" })
+    Optional<Employee> findById(Long id);
     
 }
